b4a3ed36edcd5ec4b0c55b318555dd81ca29acfe
Roy <roy860629@gmail.com>
2021-10-03 11:03:50 +0800

    新增 app.js檔案更新

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 var createError = require('http-errors');
 var express = require('express');
+var bodyParser = require('body-parser');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var db = require('./db/db')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// 定義頁面
+var loginRouter = require('./routes/login');
+var signupRouter = require('./routes/signup');
+var memberRouter = require('./routes/member');
+var memberdataRouter = require('./routes/memberdata');
+var chatRouter = require('./routes/chat');
 
 var app = express();
+app.use(bodyParser.json());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,9 +25,27 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// 頁面設置
+app.use('/login', loginRouter);
+app.use('/signup', signupRouter);
+app.use('/member', memberRouter);
+app.use('/memberdata', memberdataRouter);
+app.use('/chat', chatRouter);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.post('/add', function(req, res){
+  var body = req.body
+  console.log(body)
+    var sql = `call fsp_member_add(?, ?, ?, ?, ?);`
+    var data = [body.account,body.password,body.full_name,body.nickname,body.email]
+    db.exec(sql, data, function(results, fields) {
+      console.log(results)
+  })
+})
+
+app.post('/login')
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -32,7 +57,7 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  
   // render the error page
   res.status(err.status || 500);
   res.render('error');
