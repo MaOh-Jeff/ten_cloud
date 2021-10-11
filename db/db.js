49a0ff8b7af5bb4d6a0c96dd40de56ee2f008842
MaOh_Jeff <blackshadow1317@gmail.com>
2021-10-12 00:15:18 +0800

    [修改] 資料庫連線

--- db/db.js
+++ db/db.js
@@ -2,11 +2,11 @@ var mysql = require('mysql');
 // 建立資料庫連線
 exports.exec = (sql,data,callback) => {
     const connection = mysql.createConnection({
-        host:'localhost',
+        host: "tencloud-20210929.cqycwxgazyr4.us-east-2.rds.amazonaws.com",
         port: 3306,
-        user:'root',
-        password:'root',
-        database:'tencloudtest',
+        user:'web_server',
+        password:'a123456',
+        database:'ten_cloud',
         multipleStatements: true,
     });
     connection.connect();
